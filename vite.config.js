dbf86461ad8f1b1cd11c1eb0e1d4e2c38b7029f1
azinho <dima.statnyy@gmail.com>
2024-07-17 12:02:31 +0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import copy from 'rollup-plugin-copy';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
   plugins: [
     vue(),
-    vueJsx(),
     copy({
       targets: [
         {
